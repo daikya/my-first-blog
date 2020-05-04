9f6463557dc49926d73f2e716ff58ee12a62462a
Ubuntu <takesuzu@Ubuntu1804LTS02vm.xopam0y2twcuvhk4jt4r4ugnza.lx.internal.cloudapp.net>
2020-05-04 16:46:10 +0900

    Changed the HTML for the site.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from .models import Post
 # Register your models here.
+# Takeya
 admin.site.register(Post)
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+# urlパターンの追加。
 urlpatterns = [
         path('', views.post_list, name='post_list'),
 ]
