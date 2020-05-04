ead43fdf31effe7148262e347ab9de591889f1b0
Ubuntu <takesuzu@Ubuntu1804LTS02vm.xopam0y2twcuvhk4jt4r4ugnza.lx.internal.cloudapp.net>
2020-05-04 19:39:33 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-# urlパターンの追加。
+
 urlpatterns = [
-        path('', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
